a1c9ca94b9b9f6e9f23513e37d5e0d36bd45d096
Timothy Kovacic <computerchair.circlepines@gmail.com>
2016-10-09 16:24:48 -0400

    push for update

--- RemoteSystemsTempFiles/LOCALHOST/c/dev/toolswork/multiplayer-game/client/js/core.js
+++ RemoteSystemsTempFiles/LOCALHOST/c/dev/toolswork/multiplayer-game/client/js/core.js
@@ -55,7 +55,7 @@ core = function() {
 		}
 		
 		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-			//
+			window.location.href = 'tdk-portfolio.herokuapp.com'
 		}
 	};
 };
