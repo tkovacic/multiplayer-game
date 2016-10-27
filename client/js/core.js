268a13f25792d5e7faf829818e24d9310332b31c
Timothy Kovacic <computerchair.circlepines@gmail.com>
2016-10-27 02:36:15 -0400

    Extracted classes and server/client side bandwidth optimization

--- client/js/core.js
+++ client/js/core.js
@@ -3,30 +3,6 @@ $( document ).ready(function() { //Init scripts when page is done loading
 	APP.init();
 });
 
-player_class = function(input_id, input_username, input_pressingRight, 
-		input_pressingLeft, input_pressingUp, input_pressingDown, 
-		input_clickFire, input_right, input_status, input_source, 
-		input_frameIndex, input_tickCount, input_x, input_y) {
-	self.id = input_id;
-	self.username = input_username;
-	self.pressingRight = input_pressingRight;
-	self.pressingLeft = input_pressingLeft;
-	self.pressingUp = input_pressingUp;
-	self.pressingDown = input_pressingDown;
-	self.clickFire = input_clickFire;
-	self.right = input_right;
-	self.status = input_status;
-	self.img = {
-		source:input_source,
-		frameIndex:input_frameIndex,
-		tickCount:input_tickCount,
-	}
-	self.x = input_x;
-	self.y = input_y;
-	
-	return self;
-}
-
 core = function() {
 	var $this = this;
 	//this.local_url = 'tdk-portfolio-herokuapp.com';
